65fd79c948a9d6c6d276bfb92606de65f19f86e2
Alex Günsche <mail@lxg.de>
2015-11-16 22:58:45 +0100

    migrate formatters to new pluggable structure

---
+++ Annotation/Formatter/AbstractFormatterMeta.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package    agitation/api
+ * @link       http://github.com/agitation/AgitApiBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\ApiBundle\Annotation\Formatter;
+
+use Agit\CommonBundle\Annotation\SerializableAnnotationInterface;
+use Agit\CommonBundle\Annotation\SerializableAnnotationTrait;
+
+/**
+ * @Annotation
+ */
+abstract class AbstractFormatterMeta implements SerializableAnnotationInterface
+{
+    use SerializableAnnotationTrait;
+}
---
+++ Annotation/Formatter/Formatter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package    agitation/api
+ * @link       http://github.com/agitation/AgitApiBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\ApiBundle\Annotation\Formatter;
+
+use Agit\PluggableBundle\Strategy\PluginInterface;
+
+/**
+ * @Annotation
+ */
+class Formatter extends AbstractFormatterMeta implements PluginInterface
+{
+    /**
+     * @var The MIME type of documents produced by the formatter
+     */
+    protected $mimeType;
+
+    /**
+     * @var format name, also file extension
+     */
+    protected $format;
+}
