a5fd88890df174c4c0496362225d09acf3625db7
Alex Günsche <mail@lxg.de>
2015-11-16 21:42:37 +0100

    move formatters to ApiFormatter namespace

---
+++ Plugin/ApiFormatter/JsonpFormatter.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package    agitation/api
+ * @link       http://github.com/agitation/AgitApiBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\ApiBundle\Plugin\ApiFormatter;
+
+use Agit\ApiBundle\Exception\IncompatibleFormatterException;
+
+class JsonpFormatter extends JsonFormatter
+{
+    static protected $mimeType = 'application/javascript';
+
+    static protected $format = 'jsonp';
+
+    protected function getHttpHeaders()
+    {
+        // extra check to prevent SOP circumvention
+        if (!$this->endpoint->getMeta('Security')->get('allowCrossOrigin'))
+            throw new IncompatibleFormatterException("This endpoint does not allow cross-origin requests.");
+
+        return parent::getHttpHeaders();
+    }
+
+    protected function getHttpContent()
+    {
+        $httpContent = parent::getHttpContent();
+        $callbackName = $this->request->get('callback') ?: 'jsonpCallback';
+
+        if (preg_match('|[^a-z0-9_-]|i', $callbackName))
+            throw new IncompatibleFormatterException("The callback function name is invalid.");
+
+        return "$callbackName($httpContent);";
+    }
+}
