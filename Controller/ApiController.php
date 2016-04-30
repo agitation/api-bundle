64d357eff3b2076724ae3cc34e3f4525855d5a86
Alexander Günsche <mail@lxg.de>
2016-04-30 22:09:27 +0200

    better exception handling

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -9,6 +9,7 @@
 
 namespace Agit\ApiBundle\Controller;
 
+use Agit\IntlBundle\Translate;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,34 +28,44 @@ class ApiController extends Controller
             $this->setLocale();
             $this->checkHeaderAuth();
 
-            $endpointService = $this->container->get('agit.api.endpoint');
-            $formatterService = $this->container->get('agit.api.formatter');
+            $endpointService = $this->container->get("agit.api.endpoint");
+            $formatterService = $this->container->get("agit.api.formatter");
 
             if (!$formatterService->formatExists($_ext))
                 throw new BadRequestException("Invalid format.");
 
             $endpoint = $endpointService->createEndpoint("$namespace/$class.$method", $request);
 
-            if (!$endpoint->getMeta('Security')->get('allowCrossOrigin'))
-                $this->container->get('agit.api.csrf')->checkToken($this->getCsrfToken());
+            if (!$endpoint->getMeta("Security")->get("allowCrossOrigin"))
+                $this->container->get("agit.api.csrf")->checkToken($this->getCsrfToken());
 
             $endpoint->setupEnvironment();
 
-            if ($request->getMethod() !== 'OPTIONS')
+            if ($request->getMethod() !== "OPTIONS")
             {
                 $endpoint->executeCall();
 
-                $response = $this->container->get('agit.api.formatter')
+                $response = $this->container->get("agit.api.formatter")
                     ->getFormatter($_ext, $endpoint, $request)->getResponse();
             }
 
-            if ($endpoint->getMeta('Security')->get('allowCrossOrigin'))
-                $response->headers->set('Access-Control-Allow-Origin', "*");
+            if ($endpoint->getMeta("Security")->get("allowCrossOrigin"))
+                $response->headers->set("Access-Control-Allow-Origin", "*");
         }
         catch (\Exception $e)
         {
-            $response->setContent($e->getMessage());
-            $response->setStatusCode($e instanceof ApiException ? $e->getHttpStatus() : 500);
+            $isDebug = $this->container->getParameter("kernel.debug");
+            $isApiException = $e instanceof ApiException;
+
+            $content = $isDebug || $isApiException
+                ? $e->getMessage()
+                : Translate("Sorry, there has been an internal error. The administrators have been notified and will fix this as soon as possible.");
+
+            if ($isDebug)
+                $content .= "\n\n" . $e->getTraceAsString();
+
+            $response->setContent($content);
+            $response->setStatusCode($isApiException ? $e->getHttpStatus() : 500);
             $response->headers->set("Content-Type", "text/plain; charset=UTF-8", true);
         }
 
@@ -66,35 +77,35 @@ class ApiController extends Controller
 
     private function checkHeaderAuth()
     {
-        if ($this->container->has('agit.user'))
+        if ($this->container->has("agit.user"))
         {
-            $username = isset($_SERVER['HTTP_X_USER']) ? $_SERVER['HTTP_X_USER'] : '';
-            $password = isset($_SERVER['HTTP_X_PASSWORD']) ? $_SERVER['HTTP_X_PASSWORD'] : '';
+            $username = isset($_SERVER["HTTP_X_USER"]) ? $_SERVER["HTTP_X_USER"] : "";
+            $password = isset($_SERVER["HTTP_X_PASSWORD"]) ? $_SERVER["HTTP_X_PASSWORD"] : "";
 
             if ($username && $password)
-                $this->container->get('agit.user')->authenticate($username, $password);
+                $this->container->get("agit.user")->authenticate($username, $password);
         }
     }
 
     private function setLocale()
     {
-        $localeService = $this->container->get('agit.intl.locale');
+        $localeService = $this->container->get("agit.intl.locale");
 
-        $locale = (isset($_REQUEST['locale']) && in_array($_REQUEST['locale'], $localeService->getAvailableLocales()))
-            ? $_REQUEST['locale']
-            : 'en_GB';
+        $locale = (isset($_REQUEST["locale"]) && in_array($_REQUEST["locale"], $localeService->getAvailableLocales()))
+            ? $_REQUEST["locale"]
+            : "en_GB";
 
         $localeService->setLocale($locale);
     }
 
     private function getCsrfToken()
     {
-        $submittedCsrfToken = '';
+        $submittedCsrfToken = "";
 
-        if (isset($_SERVER['HTTP_X_TOKEN']))
-            $submittedCsrfToken = $_SERVER['HTTP_X_TOKEN'];
-        elseif (isset($_REQUEST['token']))
-            $submittedCsrfToken = $_REQUEST['token'];
+        if (isset($_SERVER["HTTP_X_TOKEN"]))
+            $submittedCsrfToken = $_SERVER["HTTP_X_TOKEN"];
+        elseif (isset($_REQUEST["token"]))
+            $submittedCsrfToken = $_REQUEST["token"];
 
         return (string)$submittedCsrfToken;
     }
