b87bc9f5041e94a90b60a6ac08011ed9ea784d63
Alexander Günsche <mail@lxg.de>
2016-09-11 10:13:26 +0200

    fix file headers

--- Common/ResponseObjectTrait.php
+++ Common/ResponseObjectTrait.php
@@ -7,14 +7,6 @@
  * @license    http://opensource.org/licenses/MIT
  */
 
-/**
- * @link       http://github.com/agitation/AgitApiBundle
- *
- * @author     Alex Günsche <http://www.agitsol.com/>
- * @copyright  2012-2015 AGITsol GmbH
- * @license    http://opensource.org/licenses/MIT
- */
-
 namespace Agit\ApiBundle\Common;
 
 use Agit\ApiBundle\Service\ResponseService;
