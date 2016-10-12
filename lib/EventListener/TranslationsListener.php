7ec0a198d710a313446716ef952e1f462e1da9ee
Alexander Günsche <mail@lxg.de>
2016-10-12 16:24:56 +0200

    only consider object’s own properties for translations

--- lib/EventListener/TranslationsListener.php
+++ lib/EventListener/TranslationsListener.php
@@ -11,47 +11,67 @@ namespace Agit\ApiBundle\EventListener;
 
 use ReflectionClass;
 use Agit\ApiBundle\Service\ObjectMetaService;
+use Agit\BaseBundle\Service\ClassCollector;
 use Agit\IntlBundle\Event\BundleTranslationsEvent;
 use Gettext\Translation;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Doctrine\Common\Annotations\Reader;
 
 class TranslationsListener
 {
-    private $objectMeta;
+    private $kernel;
 
-    public function __construct(ObjectMetaService $objectMeta, KernelInterface $kernel)
+    private $classCollector;
+
+    private $annotationReader;
+
+    public function __construct(KernelInterface $kernel, ClassCollector $classCollector, Reader $annotationReader)
     {
-        $this->objectMeta = $objectMeta;
         $this->kernel = $kernel;
+        $this->classCollector = $classCollector;
+        $this->annotationReader = $annotationReader;
     }
 
     public function onRegistration(BundleTranslationsEvent $event)
     {
-        $objectNames = $this->objectMeta->getObjectNames();
-
         $bundleAlias = $event->getBundleAlias();
         $bundle = $this->kernel->getBundle($bundleAlias);
         $bundleNamespace = $bundle->getNamespace();
         $bundlePath = $bundle->getPath();
+        $classes = $this->classCollector->collect("$bundlePath/Api", false, false);
 
-        foreach ($objectNames as $objectName) {
-            $objectClass = $this->objectMeta->getObjectClass($objectName);
-
-            if (strpos($objectClass, $bundleNamespace) !== 0) {
-                continue;
-            }
-
-            $classRefl = new ReflectionClass($objectClass);
+        foreach ($classes as $class) {
+            $classRefl = new ReflectionClass($class);
             $fileLocation = "@" . str_replace($bundlePath, $bundleAlias, $classRefl->getFileName());
 
-            $propMetas = $this->objectMeta->getObjectPropertyMetas($objectName);
+            $traitProps = $this->getTraitProperties($classRefl);
 
-            foreach ($propMetas as $propMeta) {
-                $name = $propMeta->get("Name");
+            foreach ($classRefl->getProperties() as $propRefl) {
+                if ($propRefl->class !== $class || array_key_exists($propRefl->name, $traitProps))
+                    continue;
+
+                $name = $this->annotationReader->getPropertyAnnotation($propRefl, "Agit\ApiBundle\Annotation\Property\Name");
+
+                if (!$name)
+                    continue;
+
                 $translation = new Translation($name->get("context"), $name->get("value"));
                 $translation->addReference($fileLocation);
                 $event->addTranslation($translation);
+                $names[] = $name->get("value");
             }
         }
+    }
+
+    private function getTraitProperties(ReflectionClass $classRefl)
+    {
+        $traitProps = [];
+
+        foreach ($classRefl->getTraits() as $traitRefl) {
+            $traitProps += $traitRefl->getDefaultProperties();
+            $traitProps += $this->getTraitProperties($traitRefl);
+        }
+
+        return $traitProps;
     }
 }
