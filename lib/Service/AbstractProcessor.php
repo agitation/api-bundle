f56915858229a4b1b02cd90f7bf13632b79d0726
Alexander Günsche <mail@lxg.de>
2016-11-08 20:43:31 +0100

    re-activate API generation on cache warming

--- lib/Service/AbstractProcessor.php
+++ lib/Service/AbstractProcessor.php
@@ -13,10 +13,12 @@ use Agit\ApiBundle\Annotation\Annotation;
 use Agit\ApiBundle\Annotation\Depends;
 use Agit\BaseBundle\Exception\InternalErrorException;
 use Agit\BaseBundle\Service\ClassCollector;
+use Doctrine\Common\Cache\Cache;
 use ReflectionClass;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
-abstract class AbstractProcessor
+abstract class AbstractProcessor implements CacheWarmerInterface
 {
     private $entries = [];
 
@@ -44,6 +46,21 @@ abstract class AbstractProcessor
         }
     }
 
+    /**
+     * Warms up the cache, required by CacheWarmerInterface.
+     */
+    public function warmUp($cacheDir)
+    {
+        $this->process();
+    }
+
+    /**
+     * required by CacheWarmerInterface.
+     */
+    public function isOptional()
+    {
+        return true;
+    }
     protected function addEntry($key, $entry)
     {
         $this->entries[$key] = $entry;
