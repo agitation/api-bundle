bbb4f9106adad2c5d0f5aad6a0e1f31af89eff4c
Alexander Günsche <mail@lxg.de>
2017-08-15 09:44:54 +0200

    add boolean “object” type

---
+++ lib/Api/Object/ScalarBoolean.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * @package    agitation/api-bundle
+ * @link       http://github.com/agitation/api-bundle
+ * @author     Alexander Günsche
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\ApiBundle\Api\Object;
+
+use Agit\ApiBundle\Annotation\Object;
+use Agit\ApiBundle\Annotation\Property;
+
+/**
+ * @Object\Object(namespace="common.v1", scalar=true)
+ *
+ * A simple boolean value.
+ */
+class ScalarBoolean extends AbstractValueObject
+{
+    /**
+     * @Property\Name("Value")
+     * @Property\BooleanType
+     */
+    public $_;
+}
